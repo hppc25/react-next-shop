89cde748dc81b8c5a6b37bbb8ed9b0cc1e0353a2
Helder Correia <helder.patrick25@gmail.com>
2022-09-10 13:43:13 +0100

    Integration with Stripe -  Format Price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('en-GB', {
+        style: 'currency',
+        currency: 'GBP'
+      }).format(price.unit_amount / 100),
     }
   })
 
