e669e6a89179a73fc9ff642d157237242651b823
Helder Correia <helder.patrick25@gmail.com>
2022-09-10 16:44:01 +0100

    Product & Checkout - Success Structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-    return (
-      <h1>Success</h1>
-    )
-  }
+  return (
+    <SuccessContainer>
+      <h1>Purchase made</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Helder Correia</strong>, yout <strong>T- shir</strong>it is on its way to your house.
+      </p>
+
+      <Link href="/">
+          Back to catalog
+      </Link>
+    </SuccessContainer>
+  )
+}
