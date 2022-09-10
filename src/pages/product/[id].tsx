5724d55f7fc002c5c19773bbd0a6c266f3e7cdf9
Helder Correia <helder.patrick25@gmail.com>
2022-09-10 14:27:32 +0100

    Product & Checkout - Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>T-shirt X</h1>
+        <span>35.00 £</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
+
+        <button>
+          Buy
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
