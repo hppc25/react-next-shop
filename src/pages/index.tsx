5f584b473d166ece1494cd19a918031330681ae9
Helder Correia <helder.patrick25@gmail.com>
2022-09-10 09:50:05 +0100

    Visual Structure - Configuring Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$purple',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
 
 export default function Home() {
   return (
-<h1>Hello Nextjs</h1>
+    <Button>
+      <span>Test 123</span>
+      Send
+    </Button>
   )
 }
