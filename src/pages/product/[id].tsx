883120be72be669dc48d46cf3bb037ba0f46513f
Helder Correia <helder.patrick25@gmail.com>
2022-09-10 15:13:44 +0100

    Product & Checkout - Loading product data

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,24 +1,62 @@
-import { useRouter } from "next/router"
+import { GetStaticProps } from "next"
+import Image from "next/future/image";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>T-shirt X</h1>
-        <span>35.00 £</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
+        <p>{product.description}</p>
 
         <button>
-          Buy
+          Buy now
         </button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('en-GB', {
+          style: 'currency',
+          currency: 'GBP'
+        }).format(price.unit_amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hours
+  }
 }
